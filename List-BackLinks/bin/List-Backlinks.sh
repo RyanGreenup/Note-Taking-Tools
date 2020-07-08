f0e110ef91fc59a478f1ac3c8aeaebcf7dada9e5
Ryan Greenup <exogenesis@protonmail.com>
2020-07-08 23:22:33 +1000

    Fixed -ol in Script

--- List-BackLinks/bin/List-Backlinks.sh
+++ List-BackLinks/bin/List-Backlinks.sh
@@ -8,5 +8,4 @@ command -v xclip >/dev/null 2>&1 || { echo >&2 "I require xclip but it's not ins
 
 
 term=$(xclip -selection clipboard -o | xargs basename |  cut -f 1 -d '.')
-rg --pcre2 -e "(?<=\]\().+$term\.md(?=\))" -e "\[\[$term\]\]" -e "\[\[$term.*\]\]" ~/Notes/MD/notes -t markdown -o | \
-    sed s+:+\ + | sd ' .*' ''
+rg --pcre2 -e "(?<=\]\().+$term\.md(?=\))" -e "\[\[$term\]\]" -e "\[\[$term.*\]\]" ~/Notes/MD/notes -t markdown -ol
