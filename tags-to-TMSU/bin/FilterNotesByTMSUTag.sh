1a78ad1863ff7731796e538e590b47e049639358
ryangreenup <exogenesis@protonmail.com>
2020-07-09 20:57:31 +1000

    Returned Matches each to a new line

--- tags-to-TMSU/bin/FilterNotesByTMSUTag.sh
+++ tags-to-TMSU/bin/FilterNotesByTMSUTag.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
-# set -euo pipefail
-NOTESDIR="$HOME/Notes/MD/notes"
 
-cd $NOTESDIR
+## Use the current Directory because TMSU will return
+## Relative Links from that directory and those
+## links should work from wherever this is run
+
+if [[ $1 != '' ]]; then
+    NOTE_DIR=$1
+else
+    NOTE_DIR='./'
+fi
 
 command -v rg >/dev/null 2>&1 || { echo >&2 "I require ripgrep but it's not installed.  Aborting."; exit 1; }
 command -v sd >/dev/null 2>&1 || { echo >&2 "I require sd (sed replacement) but it's not installed.  Aborting."; exit 1; }
 command -v xclip >/dev/null 2>&1 || { echo >&2 "I require xclip but it's not installed.  Aborting."; exit 1; }
 command -v tmsu >/dev/null 2>&1 || { echo >&2 "I require TMSU but it's not installed.  Aborting."; exit 1; }
-command -v perl >/dev/null 2>&1 || { echo >&2 "I require perl but it's not installed.  Aborting."; exit 1; }
+
 
 
 
@@ -35,7 +41,7 @@ $MatchingFiles
 
 FilterTags() {
 MatchingFiles=$(tmsu files "$ChosenTags")
-ConcurrentTags=$(tmsu tags $MatchingFiles | cut -f 2 -d ':' | perl -pe 's/(?<=[^\\])\ /\n/g' | sort | uniq | sort -nr )
+ConcurrentTags=$(tmsu tags $MatchingFiles | cut -f 2 -d ':' | space2NewLine | sort | uniq | sort -nr )
 ChosenTags="$ChosenTags $(echo "$ConcurrentTags" | fzf)"
 ChosenTags=$(echo "$ChosenTags" | sort | uniq )
 MatchingFiles=$(tmsu files "$ChosenTags")
@@ -70,8 +76,13 @@ read -d '' -n1 -s conTagQ
      FilterTags
  fi
 
- echo $MatchingFiles
+ echo $MatchingFiles | space2NewLine
 
+}
+
+space2NewLine() {
+    command -v perl >/dev/null 2>&1 || { echo >&2 "I require perl but it's not installed.  Aborting."; exit 1; }
+    perl -pe 's/(?<=[^\\])\ /\n/g'
 }
 
 FilterTags
